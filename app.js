8c8576a68113a688b5f658ab53a5b5a1d7c912b9
Daniel McQuiston <daniel.pro@dehugo.net>
2015-06-09 16:13:35 -0600

    Simplified code by using the cookie store feature of the request library.

--- app.js
+++ app.js
@@ -1,98 +1,28 @@
 "use strict";
 
-var config   = require('./config');
-var fs       = require('fs');
-var URL      = require('url');
-var request  = require('request');
-var cheerio  = require('cheerio');
-
-var Cookie           = require('cookiejar'),
-	CookieAccessInfo = Cookie.CookieAccessInfo,
-	CookieJar        = Cookie.CookieJar,
-	Cookie           = Cookie.Cookie;
+var config          = require('./config');
+var fs              = require('fs');
+var URL             = require('url');
+var request         = require('request');
+var cheerio         = require('cheerio');
+var FileCookieStore = require('tough-cookie-filestore');
 
 
-function readCookiesFromFile (cookieFile, cookieJar) {
-	try {
-		var cookies = JSON.parse( fs.readFileSync( cookieFile ).toString() );
-		if ( !(cookieJar instanceof CookieJar) ) {
-			cookieJar = CookieJar();
-		}
-		cookieJar.setCookies( cookies );
-	} catch ( err ) {
-		console.log("Error reading file: "+cookieFile);
-		if ( err instanceof SyntaxError ) {
-			fs.writeFile( cookieFile, "{}" );
-		} else {
-			console.log(err);
-		}
-	}
-
-	return cookieJar;
-}
+var cookieJar = request.jar(new FileCookieStore(config.filename.cookies));
+request = request.defaults({jar: cookieJar});
 
 
-function writeCookiesToFile (cookieFile, cookieJar, accessInfo) {
-	try {
-		var cookies = [];
-		(cookieJar.getCookies( accessInfo )).forEach(function(item){
-			cookies.push( item.toString() );
-		});
-		fs.writeFile( cookieFile, JSON.stringify( cookies ) );
-	} catch ( err ) {
-		console.log(err);
-	}
-}
-
-
-function setCookieHeader (cookies, headers, url) {
-	var accessInfo = CookieAccessInfo( url.hostname, url.pathname );
-
-	if ( cookies.getCookies( accessInfo ).length > 0 ) {
-		headers["Cookie"] = cookies.getCookies( accessInfo ).toValueString();
-	}
-}
-
-
-function makeRequest (cookies, options, callback) {
-	setCookieHeader(cookies, options.headers, options.url);
-
-	request(options, function(error, response, html){
-		if(!error){
-			if ( typeof response.headers['set-cookie'] != "undefined" ) {
-				var respCookies = response.headers['set-cookie'];
-				try {
-					cookies.setCookies( respCookies, options.url.hostname, options.url.pathname);
-				} catch (err) {
-					console.log("Error parsing cookie header");
-					console.log(err);
-				}
-			}
-		} else {
+(function(targetUrl){
+	request(targetUrl.href, function(error, response, html){
+		if(error){
 			console.log("There was an error in the request.");
 			throw error;
+		} else {
+			var $ = cheerio.load(html);
+			console.log($("form").attr("action"));
+			console.log($("form").serializeArray());
+
+			console.log(JSON.stringify(cookieJar));
 		}
-		callback(error, response, html);
 	});
-};
-
-
-(function(cookieFile, targetUrl){
-	var options = {
-		url: targetUrl,
-		headers: {}
-	},
-		cookies = readCookiesFromFile(cookieFile, {});
-
-	makeRequest(cookies, {url:targetUrl, headers:{}}, function(err, resp, html){
-		console.log("Returned from makeRequest");
-		var $ = cheerio.load(html);
-		console.log($("form"));
-	});
-
-	writeCookiesToFile(
-		cookieFile,
-		cookies,
-		CookieAccessInfo( targetUrl.hostname, targetUrl.pathname )
-	);
-}(config.filename.cookies, URL.parse(config.url.login)));
+}(URL.parse(config.url.login)));
